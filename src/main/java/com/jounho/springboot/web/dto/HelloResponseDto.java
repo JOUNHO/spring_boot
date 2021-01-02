1005f87e7c14a47bb67d6b1dbe9eee2690386537
jwh7371 <dnsgh9177@naver.com>
2021-01-03 04:23:55 +0900

    "Add test_code [jun,rb,aj]"

---
+++ src/main/java/com/jounho/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.jounho.springboot.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
