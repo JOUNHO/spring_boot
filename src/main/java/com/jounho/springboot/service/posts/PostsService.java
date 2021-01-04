b41566a6bccdd01826e48a85068563c8a64d493e
jwh7371 <dnsgh9177@naver.com>
2021-01-04 23:16:35 +0900

    Add JPA [TEST]

---
+++ src/main/java/com/jounho/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.jounho.springboot.service.posts;
+
+import com.jounho.springboot.domain.posts.PostsRepository;
+import com.jounho.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/test/java/com/jounho/springboot/web/PostsApiControllerTest.java
@@ -0,0 +1,6 @@
+package com.jounho.springboot.web;
+
+import org.springframework.http.ResponseEntity;
+
+public class PostsApiControllerTest {
+}
