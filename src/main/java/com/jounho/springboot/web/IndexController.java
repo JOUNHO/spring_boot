6af39c35d318918cb08163ebd5cee4fa8e601a61
jwh7371 <dnsgh9177@naver.com>
2021-01-07 16:24:01 +0900

    "Update 수정,삭제"

--- src/main/java/com/jounho/springboot/web/IndexController.java
+++ src/main/java/com/jounho/springboot/web/IndexController.java
@@ -25,9 +25,9 @@ public class IndexController {
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id,Model model){
-        PostsResponseDto dto =postsService.findById(id);
-        model.addAttribute("post",dto);
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
 
         return "posts-update";
     }
