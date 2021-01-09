0a5690e5d170a231321f0a2322cd9596e481e3d8
jwh7371 <dnsgh9177@naver.com>
2021-01-09 09:57:30 +0900

    "Add google_login"

--- src/main/java/com/jounho/springboot/web/IndexController.java
+++ src/main/java/com/jounho/springboot/web/IndexController.java
@@ -1,4 +1,5 @@
 package com.jounho.springboot.web;
+import com.jounho.springboot.config.auth.dto.SessionUser;
 import com.jounho.springboot.service.posts.PostsService;
 import com.jounho.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +8,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts",postsService.findAllDesc());
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
+
 
     @GetMapping("/posts/save")
     public String postsSave(){
