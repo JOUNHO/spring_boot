0d1b4e8242375f110f86168c033cbc420fdafdc1
jwh7371 <dnsgh9177@naver.com>
2021-01-10 13:59:17 +0900

    "Add Naver_login,Update test"

--- src/main/java/com/jounho/springboot/web/IndexController.java
+++ src/main/java/com/jounho/springboot/web/IndexController.java
@@ -1,4 +1,5 @@
 package com.jounho.springboot.web;
+import com.jounho.springboot.config.auth.LoginUser;
 import com.jounho.springboot.config.auth.dto.SessionUser;
 import com.jounho.springboot.service.posts.PostsService;
 import com.jounho.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
